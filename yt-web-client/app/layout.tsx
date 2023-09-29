1d06cf959cebf4185950550d06df3d2473ac7656
aman-senpai <jemscollin1@gmail.com>
2023-09-29 23:21:38 +0530

    Create next app for the frontend of the application

---
+++ yt-web-client/app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "My-Tube",
+    description:
+        "A scalable youtube application which supports youtube video uploading and anyone can watch it.",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
